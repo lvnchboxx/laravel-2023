0368ce796a2c8db986af962d58db3dde80186ba8
lvnchboxx <rangga.aulia@icloud.com>
2023-05-11 12:17:27 +0700

    tried to make email trap

--- chiperblade/app/Providers/EventServiceProvider.php
+++ chiperblade/app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ChirpCreated;
+use app\Listeners\SendChirpCreatedNotifications;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
